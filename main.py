9361f49e7f89eef1278680cbdfc7beaec5d26d9a
Nataleia Orlovska <nataleia.orlovska@gmail.com>
2023-03-15 18:43:39 +0200

    draft 3

--- main.py
+++ main.py
@@ -22,21 +22,11 @@ async def main():
     try:
         days = input()
         data = [datetime.now().date() - timedelta(days = i) for i in range(int(days))]
-        # asyncio.create_task(get_info(data))
-
-        loop = asyncio.get_running_loop()
-
-        with ThreadPoolExecutor(int(days)) as pool:
-            futures = [loop.run_in_executor(pool, get_info, d) for d in data]
-            result = await asyncio.gather(*futures, return_exceptions=True)
-            return result
-        # async for i in days:
-        #     await get_info(datetime.now().date()+datetime(days=i).date())
+        task = [asyncio.create_task(get_info(d)) for d in data]
+        await asyncio.wait(task)
     except ValueError as err:
         print(f"This is not number! {err}")
 
 if __name__ == "__main__":
-    # if platform.system() == 'Windows':
-    #     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
     r = asyncio.run(main())
     print(r)
