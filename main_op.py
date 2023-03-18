433c64a13924af3c3477f7c3950d948a049343ce
Nataleia Orlovska <nataleia.orlovska@gmail.com>
2023-03-18 20:28:53 +0200

    Version 1.0

--- main_op.py
+++ main_op.py
@@ -5,28 +5,33 @@ import asyncio
 from datetime import datetime, timedelta
 from concurrent.futures import ThreadPoolExecutor
 import logging
+import json
 
 async def get_info(days: datetime):
     async with aiohttp.ClientSession() as session:
         s = f"{days.day}.0{days.month}.{days.year}"
         try:
             async with session.get(f"https://api.privatbank.ua/p24api/exchange_rates?json&date={s}") as response:
-                # if response.status == 200:
-                #     result = await response.json()
-                #     return result
-                # else:
-                #     logging.error(f"{response.status}")
-                print("Status:", response.status)
-                print("Content-type:", response.headers['content-type'])
-                print('Cookies: ', response.cookies)
+                result = await response.json()
+                print(f"date: {s}")
+                print_info(result)
+                return result
         except aiohttp.ClientConnectionError as er:
             logging.error("Connection error {er}")
-        return None
             
 
-# def print_info(text):
-#     res = str(text).split(',')
-#     print(res)
+def print_info(text):
+    for k in text.keys():
+        if k == "exchangeRate":
+            print("EUR")
+            print("sale:  ", text[k][8]['saleRateNB'])
+            print("purchase:  ", text[k][8]['purchaseRateNB'])
+            print("USD")
+            print("sale:  ", text[k][23]['saleRateNB'])
+            print("purchase:  ", text[k][23]['purchaseRateNB'])
+            print("-----------------------------------------------")
+
+
 
 async def main():
     try:
@@ -45,5 +50,3 @@ if __name__ == "__main__":
     if platform.system() == 'Windows':
         asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
     r = asyncio.run(main())
-    print(r)
-    # print_info(r)
